8a3d06da8c7bb822c853183a96526735f820f2b6
Girigowda9711 <girigowda9711@gmail.com>
2022-12-16 11:40:18 +0530

    Changed the logic from int i=len to int i=len-1

--- src/main/java/com/CommNProgram/com/CommNProgram/ReverseAStirn.java
+++ src/main/java/com/CommNProgram/com/CommNProgram/ReverseAStirn.java
@@ -6,7 +6,7 @@ public class ReverseAStirn {
 		String s="Girigowda";
 		String rev="";
 		int len = s.length();
-		for(int i=len;i>=0;i--) {
+		for(int i=len-1;i>=0;i--) {
 			rev=rev+s.charAt(i);
 		}
 		System.out.println(rev);
