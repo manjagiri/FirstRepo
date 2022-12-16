bfaed004ca144fc171fba32268a8deee89f4e778
Girigowda9711 <girigowda9711@gmail.com>
2022-12-16 10:29:39 +0530

    Second programm is done

---
+++ src/test/java/com/CommNProgram/com/CommNProgram/FirstTest.java
@@ -0,0 +1,12 @@
+package com.CommNProgram.com.CommNProgram;
+
+public class FirstTest {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("its my first repository");
+	}
+	
+	
+
+}
