9f324eac5f5a3c1a6ec8cb0dd7aab86bf71a66c0
Girigowda9711 <girigowda9711@gmail.com>
2022-12-16 11:32:01 +0530

    String reverse programm

---
+++ src/main/java/com/CommNProgram/com/CommNProgram/ReverseAStirn.java
@@ -0,0 +1,28 @@
+package com.CommNProgram.com.CommNProgram;
+
+public class ReverseAStirn {
+	public static void main(String[] args) {
+		
+		String s="Girigowda";
+		String rev="";
+		int len = s.length();
+		for(int i=len;i>=0;i--) {
+			rev=rev+s.charAt(i);
+		}
+		System.out.println(rev);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+
+}
